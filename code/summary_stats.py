04f137195bea1cef92b8131a3a2ffe55a683a67a
bmassman <bmassman@gmail.com>
2017-02-20 20:07:29 -0500

    Updated summary_stats.py to better display word count and counts of null fields.

--- code/summary_stats.py
+++ code/summary_stats.py
@@ -4,72 +4,89 @@ Script to produce summary statistics for news articles.
 """
 import sqlite3
 import re
-import numpy as np
-import seaborn as sns
 from contextlib import closing
-from collections import defaultdict
 from urllib.parse import urlparse
-from operator import itemgetter
-from statistics import mean, stdev
-from itertools import chain
+import pandas as pd
+import seaborn as sns
 
 DB_FILE_NAME = 'articles.db'
-DOMAIN_ENDING = re.compile(r'\.[a-z]{2,3}$')
-DOMAIN_PREFIX = re.compile(r'^[a-z]+\.')
 
 
-def get_articles(curs):
-    """Return cursor with all articles selected."""
-    curs.execute('SELECT * FROM articles')
-    return curs.fetchall()
+def get_url_base(row):
+    """Return base url from article row."""
+    url = row['url']
+    domain_ending = re.compile(r'\.[a-z]{2,3}$')
+    domain_prefix = re.compile(r'^[a-z]+\.')
+    domains_to_keep = {'wsj', 'cnn', 'cbs', 'nbc', 'bbc', 'de'}
 
-
-def get_url_base(url):
-    """Updates count of articles by news source."""
     net_loc = urlparse(url).netloc
-    if net_loc.startswith('www.'):
+    while net_loc.startswith('www.'):
         net_loc = net_loc[4:]
     while True:
-        match = DOMAIN_ENDING.search(net_loc)
-        if not match or match.group(0) == '.wsj':
+        match = domain_ending.search(net_loc)
+        if not match or match.group(0) in domains_to_keep:
             break
-        net_loc = DOMAIN_ENDING.sub('', net_loc)
-    net_loc = DOMAIN_PREFIX.sub('', net_loc)
+        net_loc = domain_ending.sub('', net_loc)
+    net_loc = domain_prefix.sub('', net_loc)
     return net_loc
 
 
-def calculate_stats():
-    """Print statistics on article corpus."""
-    news_source_count = defaultdict(int)
-    news_source_wcs = defaultdict(list)
-    missing_authors = 0
+def count_words(row):
+    """Count words in text from article row."""
+    text = row['text']
+    return len(text.split())
 
+
+def build_df():
+    """Build dataframe with derived feilds for ."""
     with closing(sqlite3.connect(DB_FILE_NAME)) as conn:
-        curs = conn.cursor()
-        for title, authors, pub_date, url, text, tags in get_articles(curs):
-            news_source = get_url_base(url)
-            news_source_count[news_source] += 1
-            news_source_wcs[news_source].append(len(text.split()))
-            if authors == '':
-                missing_authors += 1
+        articles = pd.read_sql_query('select * from articles', conn)
 
-    return news_source_count, news_source_wcs, missing_authors
+    articles['base_url'] = articles.apply(get_url_base, axis=1)
+    articles['word_count'] = articles.apply(count_words, axis=1)
+    return articles
 
 
-def show_stats(news_source_count, news_source_wcs, missing_authors):
+def print_full(x):
+    """Print all rows in Pandas DataFrame x."""
+    pd.set_option('display.max_rows', len(x))
+    print(x)
+    pd.reset_option('display.max_rows')
+
+
+def calculate_word_count_stats(articles: pd.DataFrame):
+    """Calculate agregate word count statistics on each source's articles."""
+    by_source = articles.groupby(['base_url'])['word_count']
+    by_source = by_source.agg(['count', 'mean', 'std'])
+    by_source.sort_values('count', ascending=False, inplace=True)
+    print_full(by_source)
+
+    top_sources = by_source.head(10).index
+    sns.boxplot(x='base_url', y='word_count',
+                data=articles[articles['base_url'].isin(top_sources)])
+    sns.plt.show()
+
+
+def calculate_missing_values(articles: pd.DataFrame):
+    """Calculate count of nulls in each column."""
+    def null_fields(x):
+        return pd.Series({'no_author': (x['authors'] == '').sum(),
+                          'no_text': (x['text'] == '').sum(),
+                          'no_title': (x['title'] == '').sum()})
+
+    null_field_count = articles.groupby('base_url').apply(null_fields)
+    null_field_count = null_field_count[(null_field_count.T != 0).any()]
+    print_full(null_field_count)
+
+
+def show_stats():
     """Display statistics on articles."""
-    for site, count in sorted(news_source_count.items(), key=itemgetter(1)):
-        word_counts = news_source_wcs[site]
-        avg_len = round(mean(word_counts), 1)
-        std_len = round(stdev(word_counts), 1) if len(word_counts) > 1 else ''
-        print(f'{site} has {count} articles with mean length of {avg_len} '
-              f'words with stdev {std_len}')
+    articles = build_df()
+    calculate_word_count_stats(articles)
+    calculate_missing_values(articles)
+    sns.kdeplot(articles['word_count'], bw=1)
+    sns.plt.show()
 
-    print(f'\n\nArticles missing author: {missing_authors}')
-
-    word_counts = np.array(list(chain.from_iterable(news_source_wcs.values())))
-    sns.kdeplot(word_counts, bw=2)
 
 if __name__ == '__main__':
-    article_counts, word_counts, missing_authors = calculate_stats()
-    show_stats(article_counts, word_counts, missing_authors)
+    show_stats()
