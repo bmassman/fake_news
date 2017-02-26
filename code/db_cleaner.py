8022cb9914d0cd70ee872dd0157f3fb3d21afcb4
bmassman <bmassman@gmail.com>
2017-02-25 19:42:33 -0500

    Began db_cleaner.py.

---
+++ code/db_cleaner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Script to process article database into cleaned dataset.
+"""
+import re
+import pandas as pd
+from .summary_stats import build_df
+
+
+def fix_separators(s: str) -> str:
+    """Return s with commas inserted between fields."""
+    upper_than_lower = re.compile(r'([a-z])([A-Z])')
+    return upper_than_lower.sub(r'\1,\2', s)
+
+
+def clean_data() -> pd.DataFrame:
+    """Return cleaned dataframe of articles."""
+    articles = build_df()
+    articles['authors'] = articles['authors'].apply(fix_separators)
+    articles['tags'] = articles['tags'].apply(fix_separators)
+    return articles
