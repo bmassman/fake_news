99125cde0897b14c9863d3fa8efab131fedc20be
Jacques Choiniere <jchoinie@gmu.edu>
2017-04-08 15:34:39 -0400

    I fixed LDA sample to work with the example test data.

--- fake_news/LdaAnalysis.py
+++ fake_news/LdaAnalysis.py
@@ -48,38 +48,38 @@ def print_top_words(model, feature_names, n_top_words):
 # only one document or in at least 95% of the documents are removed.
 
 print("Loading dataset...")
-t0 = time()
-dataset = fetch_20newsgroups(shuffle=True, random_state=1,
-                             remove=('headers', 'footers', 'quotes'))
+t0 = time.time()
+#dataset = fetch_20newsgroups(shuffle=True, random_state=1,
+#                             remove=('headers', 'footers', 'quotes'))
 data_samples = dataset.data[:n_samples]
-print("done in %0.3fs." % (time() - t0))
+print("done in %0.3fs." % (time.time() - t0))
 
 # Use tf-idf features for NMF.
 print("Extracting tf-idf features for NMF...")
 tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
                                    max_features=n_features,
                                    stop_words='english')
-t0 = time()
+t0 = time.time()
 tfidf = tfidf_vectorizer.fit_transform(data_samples)
-print("done in %0.3fs." % (time() - t0))
+print("done in %0.3fs." % (time.time() - t0))
 
 # Use tf (raw term count) features for LDA.
 print("Extracting tf features for LDA...")
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
                                 max_features=n_features,
                                 stop_words='english')
-t0 = time()
+t0 = time.time()
 tf = tf_vectorizer.fit_transform(data_samples)
-print("done in %0.3fs." % (time() - t0))
+print("done in %0.3fs." % (time.time() - t0))
 
 # Fit the NMF model
 print("Fitting the NMF model with tf-idf features, "
       "n_samples=%d and n_features=%d..."
       % (n_samples, n_features))
-t0 = time()
+t0 = time.time()
 nmf = NMF(n_components=n_topics, random_state=1,
           alpha=.1, l1_ratio=.5).fit(tfidf)
-print("done in %0.3fs." % (time() - t0))
+print("done in %0.3fs." % (time.time() - t0))
 
 print("\nTopics in NMF model:")
 tfidf_feature_names = tfidf_vectorizer.get_feature_names()
@@ -92,9 +92,9 @@ lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
                                 learning_method='online',
                                 learning_offset=50.,
                                 random_state=0)
-t0 = time()
+t0 = time.time()
 lda.fit(tf)
-print("done in %0.3fs." % (time() - t0))
+print("done in %0.3fs." % (time.time() - t0))
 
 print("\nTopics in LDA model:")
 tf_feature_names = tf_vectorizer.get_feature_names()
