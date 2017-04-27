6c7cb0c979c4359d110c15c6aaa5917ee7c97328
bmassman <bmassman@gmail.com>
2017-04-26 20:18:06 -0400

    Began response surface.

--- fake_news/results/feature_analysis.py
+++ fake_news/results/feature_analysis.py
@@ -8,20 +8,36 @@ import pandas as pd
 import statsmodels.formula.api as smf
 
 
-def linear_regression(classifier: str = None):
+def get_results():
+    results_file = os.path.join('fake_news', 'results', 'results.csv')
+    return pd.read_csv(results_file)
+
+
+def linear_regression(df, classifier: str = None):
     """
     Output results of linear regression model of feature sets and learner
     against prediction accuracy.
     """
-    results_file = os.path.join('fake_news', 'results', 'results.csv')
-    df = pd.read_csv(results_file)
     if classifier:
         df = df[df['classifier'] == classifier]
-    formula = ('test_accuracy ~ classifier + tags + spell + grammar + '
+    formula = ('test_accuracy ~ tags + spell + grammar + '
                '+ word_count + tfidf + lsh + title + sentiment + 0')
     model = smf.ols(formula=formula, data=df).fit()
     print(model.summary())
 
 
+def response_surface_analysis(df):
+    features = ['tags', 'spell', 'grammar', 'word_count', 'tfidf', 'lsh',
+                'title', 'sentiment']
+    for feature in features:
+        with_feature = df[df[feature] == 1]
+        wo_feature = df[df[feature] == 0]
+        acc_diff = (with_feature['test_accuracy'].mean()
+                    - wo_feature['test_accuracy'].mean())
+        print(f'{feature}: {acc_diff}')
+
+
 if __name__ == '__main__':
-    linear_regression('Logistic Regression')
+    df = get_results()
+    linear_regression(df, 'Logistic Regression')
+    response_surface_analysis(df)
